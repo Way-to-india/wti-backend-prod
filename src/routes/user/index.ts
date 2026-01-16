f10474d9fa0c90a57c23ca359b82065e2a99df3a
Pratham Mehta <102432423+Pratham32445@users.noreply.github.com>
2026-01-16 15:28:07 +0530

    fix

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -1,13 +1,11 @@
 import AppRoutes from '@/common/appRoutes';
 import { Router } from 'express';
 import AuthRouter from './auth.routes';
-import ReviewRouter from './review.routes';
 import TourRouter from './tour.routes';
 
 const router = Router();
 
 router.use(AppRoutes.AUTH, AuthRouter);
 router.use(AppRoutes.TOUR, TourRouter);
-router.use(AppRoutes.REVIEW, ReviewRouter);
 
 export default router;
